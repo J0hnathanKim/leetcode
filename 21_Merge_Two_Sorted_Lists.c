79f666aa1d8640e10ac5c5ce270677aec2dfb7ec
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-07-21 14:39:25 +0900

    Create 21_Merge_Two_Sorted_Lists.c

---
+++ 21_Merge_Two_Sorted_Lists.c
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+ struct ListNode* createNode(int value) {
+    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+    newNode->val = value;
+    newNode->next = NULL;
+    return newNode;
+}
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode* dummy = createNode(0);
+    struct ListNode* current = dummy;
+    
+    
+    while (list1 != NULL && list2 != NULL) {
+        if (list1->val < list2->val) {
+            current->next = list1;
+            list1 = list1->next;
+        } else {
+            current->next = list2;
+            list2 = list2->next;
+        }
+        current = current->next;
+    }
+
+    
+    if (list1 != NULL) {
+        current->next = list1;
+    } else {
+        current->next = list2;
+    }
+
+   
+    struct ListNode* mergedList = dummy->next;
+    free(dummy);  
+    return mergedList;
+}
